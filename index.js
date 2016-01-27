b53bdac87a02f0ed29124fad6cb7cead1f6a3e3e
Nick DeMonner <ndemonner@gmail.com>
2016-01-27 09:11:34 -0800

    Get skeleton running

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hi there");
+const path = require("path");
+const print = require("./lib/print");
+
+const args = process.argv.slice(2);
+const commandPath = path.join(__dirname, "lib", "commands", args[0]);
+
+try {
+  require(commandPath)(args.slice(1));
+} catch(e) {
+  print.error(e.toString());
+  print.error(`There is no command named \`${args[0]}\``);
+}
